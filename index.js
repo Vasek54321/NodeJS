ac3fbd8ec313d3e57a0425794976c18ffa82489e
Vasek54321 <jachym5hanke@gmail.com>
2023-03-07 10:18:28 +0100

    03 - instalace knihoven bodyparser a moment

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const express = require("express");
 const app = express();
 /* Nastavení portu, na němž bude spuštěný server naslouchat */
 const port = 3000;
+/* Připojení externího modulu body-parser (https://www.npmjs.com/package/body-parser) -
+middleware pro parsování těla požadavku */
+const bodyParser = require("body-parser");
+/* Připojení externího modulu moment (https://momentjs.com/) - knihovna pro formátování 
+datových a časových údajů */
+const moment = require("moment");
+/* Připojení vestavěných modulů fs (práce se soubory) a path (cesty v adresářové struktuře) */
+const fs = require("fs");
+const path = require("path");
+
 /* Identifikace složky obsahující statické soubory klientské části webu */
 app.use(express.static("public"));
 /* Spuštění webového serveru */
